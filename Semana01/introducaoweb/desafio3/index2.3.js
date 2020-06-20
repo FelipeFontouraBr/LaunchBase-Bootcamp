1d6c1bba4115032622bc259e56d54da089893b46
FelipeFontouraBr <felipefontouramec@gmail.com>
2020-06-20 21:39:47 +0200

    Update index2.3.js
    Other option of resolution

--- Semana01/introducaoweb/desafio3/index2.3.js
+++ Semana01/introducaoweb/desafio3/index2.3.js
@@ -16,6 +16,24 @@ const usuarios = [
     }
 ]
 
-for (usuario of usuarios) {
-    console.log(`${usuario.nome} trabalha com ${usuario.tecnologias.join(', ')}`)
+// Crie uma função que recebe os dados de um objeto de usuário e retorna SE o usuário trabalha com CSS ou não. Essa função deve retornar um boolean true/false
+
+function checaSeUsuarioUsaCSS(usuario) {
+    // Percorra o array de tecnologias do usuário até encontrar se ele trabalha com CSS
+    // SE encontrar, retorne true da função, caso contrário retorne false
+    for(let tecnologia of usuario.tecnologias) {
+        const encontreiTecnologiaCSS = tecnologia === 'CSS'
+        if(encontreiTecnologiaCSS) {
+            return true;
+        } // Achou um dos usuarios que tem css e é true, acaba a função 'if'     
+    }
+    return false; //Retorno um falso e terminamos a função geral (Sempre precisamos de um return)
+}
+
+for (let i = 0; i < usuarios.length; i++) {
+    const usuarioTrabalhaComCss = checaSeUsuarioUsaCSS(usuarios[i]);
+
+    if(usuarioTrabalhaComCss) {
+        console.log(`O usuário ${usuarios[i].nome} trabalha com ${usuarios[i].tecnologias}`)
+    }
 }
