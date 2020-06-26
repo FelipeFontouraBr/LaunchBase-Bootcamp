3f7efbe340a60ffd62346e4169bb411d809c9052
FelipeFontouraBr <felipefontouramec@gmail.com>
2020-06-26 13:18:46 +0200

    Added logic to maxime modal

--- Semana01/frontend/desafio1/scripts.js
+++ Semana01/frontend/desafio1/scripts.js
@@ -2,10 +2,13 @@ const modalOverlay = document.querySelector('.modal-overlay')
 
 const cards = document.querySelectorAll('.card')
 
+const modal = document.querySelector('.modal')
+
+
 for (let card of cards) {
     card.addEventListener("click", function(){
         const siteId = card.getAttribute("id");
-        console.log(siteId)
+        
         modalOverlay.classList.add('active')
 
         modalOverlay.querySelector("iframe").src = `https://rocketseat.com.br/${siteId}`;
@@ -14,8 +17,12 @@ for (let card of cards) {
 
 document.querySelector('.close-modal').addEventListener('click', function(){
     modalOverlay.classList.remove('active')
+    modalOverlay.querySelector('iframe') = ``
+    modal.classList.remove('maximize')  
 })
 
 document.querySelector('.maxime-modal').addEventListener('click', function(){
-    modalOverlay.classList.contains('active')
+    if(!modal.classList.contains('maximize')){
+        modal.classList.add('maximize')
+    }   
 })
