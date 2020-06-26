27a2e28462e5bf110585e7ad34ff311420cbd842
FelipeFontouraBr <felipefontouramec@gmail.com>
2020-06-26 11:54:59 +0200

    Changed src from iframe
    Created a variable to src

--- Semana01/frontend/desafio1/scripts.js
+++ Semana01/frontend/desafio1/scripts.js
@@ -5,6 +5,7 @@ const cards = document.querySelectorAll('.cards')
 for (let card of cards) {
     card.addEventListener("click", function(){
         modalOverlay.classList.add('active')
+        modalOverlay.querySelector('iframe').src = `https://rocketseat.com.br/${siteId}`
     })
 }
 
