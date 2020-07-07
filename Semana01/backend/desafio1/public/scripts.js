4f0be5d0190f846906fcb138e56c4747dacaacad
FelipeFontouraBr <felipefontouramec@gmail.com>
2020-07-07 13:46:55 +0200

    Added external files

---
+++ Semana01/backend/desafio1/public/scripts.js
@@ -0,0 +1,35 @@
+const modalOverlay = document.querySelector('.modal-overlay')
+
+const cards = document.querySelectorAll('.card')
+
+const modal = document.querySelector('.modal')
+
+
+for (let card of cards) {
+    card.addEventListener("click", function(){
+        const siteId = card.getAttribute("id");
+        
+        modalOverlay.classList.add('active')
+
+        modalOverlay.querySelector("iframe").src = `https://rocketseat.com.br/${siteId}`;
+    })
+}
+
+
+// Maximizar conteúdo
+document.querySelector('.maxime-modal').addEventListener('click', function(){
+    if(!modal.classList.contains('maximize')){
+        modal.classList.add('maximize')
+    } else {
+        modal.classList.remove('maximize')
+    }
+     
+})
+
+// Fechar conteúdo
+document.querySelector('.close-modal').addEventListener('click', function(){
+    modalOverlay.classList.remove('active')
+    modal.classList.remove('maximize')
+    modalOverlay.querySelector('iframe') = ``      
+})
+
