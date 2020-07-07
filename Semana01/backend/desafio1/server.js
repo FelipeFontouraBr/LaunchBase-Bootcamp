c659b0ac4a67ff15797d3122734842a8d929066b
FelipeFontouraBr <felipefontouramec@gmail.com>
2020-07-07 14:16:18 +0200

    Added nunjucks

--- Semana01/backend/desafio1/server.js
+++ Semana01/backend/desafio1/server.js
@@ -1,11 +1,17 @@
 const express = require('express')
-
+const nunjucks = require('nunjucks')
 
 const server = express()
 
 
 // Setting server to search static files:
+server.use(express.static('public'))
+
 server.set("view engine", "html")
+
+nunjucks.configure("views", {
+    express: server
+})
 
 // Routes
 // Rota Raiz
