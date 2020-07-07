37de4584c0413f2d0cc6c1cbd9a4c246f9d4e9ce
FelipeFontouraBr <felipefontouramec@gmail.com>
2020-07-07 14:08:43 +0200

    Routes Created

--- Semana01/backend/desafio1/server.js
+++ Semana01/backend/desafio1/server.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const server = express()
 
+// Routes
+// Rota Raiz
+server.get("/", function(req, res){
+    return res.render("content")
+})
+
+server.get("/index", function(req, res){
+    return res.render("index")
+})
+
+
+// Start server
 server.listen(5000, function(){
     console.log('The server is running')
 })
