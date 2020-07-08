f37b1fdd866ca6f3183e6de3c038cae77a45960e
FelipeFontouraBr <felipefontouramec@gmail.com>
2020-07-08 14:10:29 +0200

    Created struture repetion

--- Semana01/Foodfy/scripts.js
+++ Semana01/Foodfy/scripts.js
@@ -3,3 +3,10 @@ const modalOverlay = document.querySelectorAll('.modal-overlay')
 // Selecionando as receitas
 const revenues = document.querySelectorAll('info-pizzas')
 
+for (let revenue of revenues) {
+    revenue.addEventListener("click", function(){
+        const videoID = revenue.getAttribute("id")
+        modalOverlay.classList.add('active')
+        modalOverlay.querySelector("iframe").src= ``;
+    })
+}
