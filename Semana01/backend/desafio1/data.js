f55a745c981d72f2e8f1cfacb847543f19de6008
FelipeFontouraBr <felipefontouramec@gmail.com>
2020-07-09 11:51:10 +0200

    Created data.js

---
+++ Semana01/backend/desafio1/data.js
@@ -0,0 +1,20 @@
+module.exports = [
+    {
+        id: "starter",
+        number: "01",
+        modules: "5 Módulos",
+        price: "Gratuito"
+    },
+    {
+        id: "launchbase",
+        number: "02",
+        modules: "24 Módulos",
+        price: "Gratuito"
+    },
+    {
+        id: "gostack",
+        number: "03",
+        modules: "20 Módulos",
+        price: "Gratuito"
+    }
+]
