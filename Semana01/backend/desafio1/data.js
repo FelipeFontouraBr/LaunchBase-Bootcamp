3030703dd0f345389e21270f33d1b0002f9989f3
FelipeFontouraBr <felipefontouramec@gmail.com>
2020-07-09 12:07:18 +0200

    Fixed error: data.js

--- Semana01/backend/desafio1/data.js
+++ Semana01/backend/desafio1/data.js
@@ -2,18 +2,21 @@ module.exports = [
     {
         id: "starter",
         number: "01",
+        img: "https://skylab.rocketseat.com.br/static/media/Starter.64c237cc.svg",
         modules: "5 Módulos",
         price: "Gratuito"
     },
     {
         id: "launchbase",
         number: "02",
+        img: "https://app.rocketseat.com.br/static/media/LaunchBase.08285320.svg",
         modules: "24 Módulos",
         price: "Gratuito"
     },
     {
         id: "gostack",
         number: "03",
+        img: "https://app.rocketseat.com.br/static/media/GoStack.83a178a0.svg",
         modules: "20 Módulos",
         price: "Gratuito"
     }
