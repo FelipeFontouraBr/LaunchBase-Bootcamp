54765451aff3b35f23de087f6e37f7d629dfabac
FelipeFontouraBr <felipefontouramec@gmail.com>
2020-07-09 11:10:25 +0200

    created "viariables" to card:
    - Card_Title
    - Card_Subtitle

--- Semana01/Foodfy/scripts.js
+++ Semana01/Foodfy/scripts.js
@@ -6,6 +6,9 @@ const revenues = document.querySelectorAll('.card')
 for (let revenue of revenues) {
     revenue.addEventListener("click", function(){
         const revenueID = revenue.getAttribute("id");
+        const cardTitle = card.querySelector('.card__content').querySelector('h2').textContent;
+        const cardSubtitle = card.querySelector('.card_subtitle').querySelector('p').textContent;
+
         modalOverlay.classList.add('active')
         modalOverlay.querySelector('img').src= `./assets/${revenueID}.png`;
     })
