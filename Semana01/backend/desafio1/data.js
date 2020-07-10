70e69d5ab4d82bade1b4d669eac0740b11073c83
FelipeFontouraBr <felipefontouramec@gmail.com>
2020-07-10 12:08:37 +0200

    Added "title" to data.js

--- Semana01/backend/desafio1/data.js
+++ Semana01/backend/desafio1/data.js
@@ -2,6 +2,7 @@ module.exports = [
     {
         id: "starter",
         number: "01",
+        title: "Start Cursos",
         card: "card1",
         img: "https://skylab.rocketseat.com.br/static/media/Starter.64c237cc.svg",
         modules: "5 Módulos",
@@ -10,6 +11,7 @@ module.exports = [
     {
         id: "launchbase",
         number: "02",
+        title: "LaunchBase Bootcamp",
         card: "card2",
         img: "https://app.rocketseat.com.br/static/media/LaunchBase.08285320.svg",
         modules: "24 Módulos",
@@ -18,6 +20,7 @@ module.exports = [
     {
         id: "gostack",
         number: "03",
+        title: "GoStack Bootcamp",
         card: "card3",
         img: "https://app.rocketseat.com.br/static/media/GoStack.83a178a0.svg",
         modules: "20 Módulos",
