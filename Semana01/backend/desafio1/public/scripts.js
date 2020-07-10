cf7e8a2ec34dda275583a10ce054ea5824ec1ccc
FelipeFontouraBr <felipefontouramec@gmail.com>
2020-07-10 13:12:46 +0200

    Added "window.location.href" to open the selected page

--- Semana01/backend/desafio1/public/scripts.js
+++ Semana01/backend/desafio1/public/scripts.js
@@ -8,14 +8,11 @@ const modal = document.querySelector('.modal')
 for (let card of cards) {
     card.addEventListener("click", function(){
         const siteId = card.getAttribute("id");
-        
-        modalOverlay.classList.add('active')
-
-        modalOverlay.querySelector("iframe").src = `https://rocketseat.com.br/${siteId}`;
+        window.location.href = `/course?id=${siteId}`
     })
 }
 
-
+/*
 // Maximizar conteúdo
 document.querySelector('.maxime-modal').addEventListener('click', function(){
     if(!modal.classList.contains('maximize')){
@@ -31,5 +28,5 @@ document.querySelector('.close-modal').addEventListener('click', function(){
     modalOverlay.classList.remove('active')
     modal.classList.remove('maximize')
     modalOverlay.querySelector('iframe') = ``      
-})
+})*/
 
