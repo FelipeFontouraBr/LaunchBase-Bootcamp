0ec993c491a6d6ea637a0971ef30806c41ba53ef
FelipeFontouraBr <felipefontouramec@gmail.com>
2020-07-13 10:58:35 +0200

    Created Routes

--- Semana01/Foodfy/server.js
+++ Semana01/Foodfy/server.js
@@ -1,6 +1,25 @@
 const express = require('express')
 const server = express()
 
+//Routes
+// Index
+server.get("/", function(req, res){
+    return res.render('index')
+})
+
+// About
+server.get("/about", function(req, res){
+    return res.render('about')
+})
+
+// Revenue
+server.get("revenue", function(req, res){
+    return res.render('revenue')
+})
+
+// Problem 404
+
+
 server.listen(5000, function(){
     console.log('The server is running')
 })
