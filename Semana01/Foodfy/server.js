d1bf30aaf395493a894a4c12e2f7be81b67937e5
FelipeFontouraBr <felipefontouramec@gmail.com>
2020-07-13 13:04:46 +0200

    Setting files statics

--- Semana01/Foodfy/server.js
+++ Semana01/Foodfy/server.js
@@ -4,6 +4,8 @@ const nunjucks = require('nunjucks')
 const server = express()
 
 // Setting server
+server.use(express.static('public'))
+
 server.set("view engine", "html")
 
 nunjucks.configure("views", {
