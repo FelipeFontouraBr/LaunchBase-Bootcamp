d1005edf9114808b14c10ad1df51016f459d765b
FelipeFontouraBr <felipefontouramec@gmail.com>
2020-07-13 13:07:07 +0200

    Fixed error: Routes

--- Semana01/Foodfy/server.js
+++ Semana01/Foodfy/server.js
@@ -19,12 +19,12 @@ server.get("/", function(req, res){
 })
 
 // About
-server.get("/about", function(req, res){
+server.get("/about.html", function(req, res){
     return res.render('about')
 })
 
 // Revenue
-server.get("revenue", function(req, res){
+server.get("/revenue.html", function(req, res){
     return res.render('revenue')
 })
 
