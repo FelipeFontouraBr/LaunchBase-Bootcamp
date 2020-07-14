cc2159d47b31fffbf2749d864026f0a166358a68
FelipeFontouraBr <felipefontouramec@gmail.com>
2020-07-14 11:11:28 +0200

    Created const in the index

--- Semana01/Foodfy/server.js
+++ Semana01/Foodfy/server.js
@@ -3,18 +3,59 @@ const nunjucks = require('nunjucks')
 
 const server = express()
 
+const information = require("./data")
+
 // Setting server
 server.use(express.static('public'))
 
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-    express: server
+    express: server,
+    autoescape: false,
+    noCache: true
 })
 
 //Routes
 // Index
 server.get("/", function(req, res){
+    const index = {
+        avatar_chef: "./assets/chef.png",
+        title_more: "Mais acessadas",
+        revenues: [
+            {
+                revenue: "./assets/burger.png",
+                title: "Triplo bacon burger",
+                subtitle: "por Jorge Relato"
+            },
+            {
+                revenue: "./assets/pizza.png",
+                title: "Pizza 4 estações",
+                subtitle: "por Fabiana Melo"
+            },
+            {
+                revenue: "./assets/espaguete.png",
+                title: "Triplo bacon burger",
+                subtitle: "por Júlia Kinoto"
+            },
+            {
+                revenue: "./assets/lasanha.png",
+                title: "Lasanha mac n' cheese",
+                subtitle: "por Juliano Vieira"
+            },
+            {
+                revenue: "./assets/doce.png",
+                title: "Docinhos pão-do-céu",
+                subtitle: "por Ricardo Golvea"
+            },
+            {
+                revenue: "./assets/asinhas.png",
+                title: "Asinhas de frango ao barbecue",
+                subtitle: "por Vania Steroski"
+            },
+        ]
+    }
+
     return res.render('index')
 })
 
