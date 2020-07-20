8549919ad38db547e7ada16a867c2102c667d9b6
FelipeFontouraBr <felipefontouramec@gmail.com>
2020-07-20 13:32:22 +0200

    Created route to index.njk

--- Fase03/lessons/routes.js
+++ Fase03/lessons/routes.js
@@ -4,3 +4,7 @@ const routes = express.Router()
 routes.get('/', function(req, res) {
     return res.redirect("/teachers")
 })
+
+routes.get('/instructors', function(req, res) {
+    return res.render("teachers/index")
+})
