74e0a3c891e1b753715f0aeb7c30edb0a4718dfa
FelipeFontouraBr <felipefontouramec@gmail.com>
2020-07-25 18:15:39 +0200

    validation with 'key'

--- Fase03/lessons/teachers.js
+++ Fase03/lessons/teachers.js
@@ -2,3 +2,20 @@
 
 const fs = require('fs')
 const data = require('./data.json')
+
+// CREATE
+exports.post = function (req, res) {
+
+    const keys = Object.keys(req.body)
+
+    for (key of keys) {
+        if (req.body[key] == "") {
+            return res.send('Por favor, preencha todos os campos')
+        }
+    }
+
+
+
+
+
+}
