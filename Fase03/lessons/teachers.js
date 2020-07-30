ff7eb4a837db51de7832064db92ba135152f250c
FelipeFontouraBr <felipefontouramec@gmail.com>
2020-07-30 14:01:02 +0200

    Fixed error: setting json

--- Fase03/lessons/teachers.js
+++ Fase03/lessons/teachers.js
@@ -35,14 +35,14 @@ exports.post = function (req, res) {
     })*/
 
     // Setting Json
-   /*fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+    fs.writeFile("data.json", JSON.stringify(req.body), function(err) {
         if(err) {
             return res.send("Erro na escrita campeão")
         }
 
-        return res.direct("/teachers")
-    })*/
+        return res.redirect("/teachers")
+    })
 
-    return res.send(req.body)
+    //return res.send(req.body)
 
 }
