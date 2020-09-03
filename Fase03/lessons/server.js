3c2f48e6bf67561919021c33e5bcc7b4ead80994
FelipeFontouraBr <felipefontouramec@gmail.com>
2020-09-03 12:06:42 +0200

    Setting server with methodoOverride

--- Fase03/lessons/server.js
+++ Fase03/lessons/server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
 const routes = require('./routes')
+const methodOverride = require('method-override')
 const server = express()
 
 // Setting server to search static files:
 server.use(express.urlencoded({ extended: true }))
 server.use(express.static('public'))
+server.use(methodOverride('_method'))
 server.use(routes)
 
 server.set("view engine", "njk")
