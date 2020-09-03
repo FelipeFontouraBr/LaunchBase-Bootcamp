5e06a4a9e43c9cde7ef4c2a69fe4a457453efc4a
FelipeFontouraBr <felipefontouramec@gmail.com>
2020-09-03 12:54:58 +0200

    Created route to put (Update)

--- Fase03/lessons/routes.js
+++ Fase03/lessons/routes.js
@@ -18,7 +18,9 @@ routes.get('/teachers/:id', teachers.show)
 
 routes.get('/teachers/:id/edit', teachers.edit)
 
-routes.post("/teachers", teachers.post)
+routes.post('/teachers', teachers.post)
+
+routes.put('/teachers', teachers.put)
 
 routes.get('/students', function(req, res){
     return res.send("students")
